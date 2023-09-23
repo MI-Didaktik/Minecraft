b396492979552317f85455d90d82b3f276c229be
Beatrice <beatrice.wellmann@gmx.de>
2023-09-23 15:06:07 +0200

    Anpassung der Controller-Klasse: Erstellen des Spielsplans inklusive variabler Button-Listener ermgöglicht.

--- Minesweeper/Controller.java
+++ Minesweeper/Controller.java
@@ -5,10 +5,15 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
 import java.io.IOException;
+import javafx.event.EventHandler;
+import java.awt.event.MouseEvent;
 
 /**
- * Beschreiben Sie hier die Klasse Spielfeld.
+ * Beschreiben Sie hier die Klasse Controller.
  * 
  * @author Tim Busch, Beatrice Wellmann 
  * @version 1
@@ -33,30 +38,11 @@ public class Controller {
 
     @FXML
     void start(ActionEvent event) throws IOException {
-        spiel = new Spiel();
+        spiel = new Spiel(this);
         spielfeld = spiel.getSpielfeld();
         fuelleSpielfeld();
     }
 
-    // @FXML
-    // void spielerHinzu(ActionEvent event) throws IOException {
-        // if(anzSpieler<1 && !TextFieldSpielerName.getText().isBlank()){
-            // Spieler s = new Spieler(TextFieldSpielerName.getText());
-            // FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/kartenhand.fxml"));
-            // Pane newLoadedPane = loader.load();
-            // kartenHand = (ControllerKH)loader.getController();
-            // kartenHand.setCroupier(c);
-            // kartenHand.setCKH(croupierKH);
-            // kartenHand.setSpieler(s);
-            // kartenHand.teilStarthand();
-
-            // GridPaneSpielFelder.add(newLoadedPane, anzSpieler+2, 0);
-            // anzSpieler++;
-            // ButtonSpielerHinzu.setVisible(false);
-            // TextFieldSpielerName.setVisible(false);
-        // }
-    // }
-    
     private void fuelleSpielfeld(){
         spielfeldGrid = new GridPane();
         Feld[][] felder = spiel.getFelder();
@@ -65,11 +51,50 @@ public class Controller {
         for (int r = 0; r < reihen; r++){
             for (int s = 0; s< spalten; s++){
                 Feld f = felder[r][s];
-                Button fbutton = new Button();
-                fbutton.setId("b"+r+s);
-                // fbutton.addEventHandler(EventType., _EventHandler<? super T>_)
-                spielfeldGrid.add(fbutton, r, s);
+                Button fButton = new Button();
+                fButton.setId("#b"+r+s);
+                mausListenerHinzufuegen(fButton,r,s);
+                spielfeldGrid.add(fButton, r, s);
             }
         }
+        //TODO: Spielfeldgröße an Grid anpassen
     }
+    
+    public void aktualisiereSpielstatus(){
+        //status auslesen
+        //label, smiley, highscore ändern
+    }
+
+    public void feldAktualisieren(Feld feld, int r, int s){
+        Image image = feld.getBild();
+        Button fButton = (Button) spielfeldGrid.getScene().lookup("#b"+r+s);
+        ImageView view = new ImageView();
+        view.setImage(image);
+        fButton.setGraphic(view); 
+    }
+
+    private void mausListenerHinzufuegen(Button fButton, int r, int s){
+        fButton.setOnMouseClicked(event ->
+                {
+                    Spielstatus status = spiel.getSpielstatus();
+                    if (status.equals(Spielstatus.NICHTGESTARTET)){
+                        spiel.setSpielstatus(Spielstatus.LAUFEND);
+                        status = spiel.getSpielstatus();
+                        //TODO: starte timer
+                    }
+                    if (status.equals(Spielstatus.LAUFEND)){
+                        // Was passiert beim linken oder rechten Mausklick?
+                        if (event.getButton() == MouseButton.PRIMARY)
+                        {
+                            fButton.setDisable(true);
+                            //TODO: bild ändern spiellogik, bombenanzahl etc
+                        } else if (event.getButton() == MouseButton.SECONDARY)
+                        {
+                            fButton.setDisable(false);
+                            //TODO: bild ändern spiellogik, bombenanzahl etc
+                        }
+                    }
+            });
+    }
+
 }
